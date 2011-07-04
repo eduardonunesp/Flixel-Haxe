6d3b4b2516a50f656a1802acf2acfd175be932d8
= <=>
2011-07-04 09:06:29 -0300

    Some libs

---
+++ org/flixel/data/assets/LibraryClasses.hx
@@ -0,0 +1,22 @@
+import flash.display.Bitmap;
+import flash.media.Sound;
+
+class Delicious_png extends Bitmap { public function new() { super(); } }
+class Digg_png extends Bitmap { public function new() { super(); } }
+class Close_png extends Bitmap { public function new() { super(); } }
+class Autotiles_png extends Bitmap { public function new() { super(); } }
+class Key_p_png extends Bitmap { public function new() { super(); } }
+class Autotiles_alt_png extends Bitmap { public function new() { super(); } }
+class Logo_corners_png extends Bitmap { public function new() { super(); } }
+class Twitter_png extends Bitmap { public function new() { super(); } }
+class Reddit_png extends Bitmap { public function new() { super(); } }
+class Key_plus_png extends Bitmap { public function new() { super(); } }
+class Stumble_png extends Bitmap { public function new() { super(); } }
+class Donate_png extends Bitmap { public function new() { super(); } }
+class Key_minus_png extends Bitmap { public function new() { super(); } }
+class Logo_light_png extends Bitmap { public function new() { super(); } }
+class Key_0_png extends Bitmap { public function new() { super(); } }
+class Logo_png extends Bitmap { public function new() { super(); } }
+class Cursor_png extends Bitmap { public function new() { super(); } }
+class Beep_mp3 extends Sound { public function new() { super(); } }
+class Flixel_mp3 extends Sound { public function new() { super(); } }
